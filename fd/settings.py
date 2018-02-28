4570e26251a09868c3626b29e67246bc5ac5852d
DeGreat Yartey <yarteydegreat2@gmail.com>
2018-02-28 14:27:49 +0000

    Added allowed hosts

--- fd/settings.py
+++ fd/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q^l5#gsh+09+wsl_kc4m*$fm0u6wge%4jj@ab*j5$@%$7c!%d0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', ]
 
 
 # Application definition
